fc2cf1b31830aa6883745d92dbbdc560dc52837a
yadson18 <yadson18@hotmail.com>
2017-12-02 20:42:19 -0300

    Concluir a classe Router e Request

--- vendor/Simple/src/Routing/Router.php
+++ vendor/Simple/src/Routing/Router.php
@@ -3,11 +3,80 @@
 
 	class Router
 	{
-		private static $defaultRoute;
+		private static $defaultController;
+
+		private static $defaultView;
+
+		private $controller;
+
+		private $view;
 
 		public static function configRoute(array $defaultRoute)
 		{
-			static::$defaultRoute = $defaultRoute;
+			if (isset($defaultRoute['controller']) && isset($defaultRoute['view'])) {
+				static::setDefaultController($defaultRoute['controller']);
+				static::setDefaultView($defaultRoute['view']);
+			}
+		}
+
+		protected function setDefaultController(string $controller)
+		{
+			static::$defaultController = ucfirst($controller);
+		}
+
+		protected function getDefaultController()
+		{
+			return static::$defaultController;
+		}
+
+		protected function setDefaultView(string $view)
+		{
+			static::$defaultView = $view;
+		}
+
+		protected function getDefaultView()
+		{
+			return static::$defaultView;
+		}
+
+		protected function setController(string $controller)
+		{
+			$this->controller = ucfirst($controller);
+		}
+
+		public function getController()
+		{
+			return $this->controller;
+		}
+
+		protected function setView(string $view)
+		{
+			$this->view = $view;
+		}
+
+		public function getView()
+		{
+			return $this->view;
+		}
+
+		public function setRoute(array $route)
+		{
+			if (isset($route['controller']) && isset($route['view'])) {
+				if (empty($route['controller'])) {
+					$this->setController(static::getDefaultController());
+					$this->setView(static::getDefaultView());
+				}
+				else {
+					if (empty($route['view'])) {
+						$this->setView('index');
+					}
+					else {
+						$this->setView($route['view']);
+					}
+					
+					$this->setController($route['controller']);
+				}
+			}
 		}
 
 		public static function getDefaultRoute(string $indexName = null)
