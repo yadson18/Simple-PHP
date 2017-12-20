5002d44646cf76121a6fe9d1eb5b1619ca038a8b
yadson18 <yadson18@hotmail.com>
2017-12-20 01:40:42 +0000

    Criando nova página de exibição

--- src/Template/Layout/default.php
+++ src/Template/Layout/default.php
@@ -15,29 +15,150 @@
 
 		<?= $this->Html->less('home.less') ?>
 		<?= $this->Html->script('less.min.js') ?>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+ 		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+		<style>  
+			.navbar {
+				margin-bottom: 0;
+				background-color: #f4511e;
+				z-index: 9999;
+				border: 0;
+				font-size: 12px !important;
+				line-height: 1.42857143 !important;
+				letter-spacing: 4px;
+				border-radius: 0;
+				font-family: Montserrat, sans-serif;
+			}
+			.navbar li a, .navbar .navbar-brand {
+				color: #fff !important;
+			}
+			.navbar-nav li a:hover, .navbar-nav li.active a {
+				color: #f4511e !important;
+				background-color: #fff !important;
+			}
+			.navbar-default .navbar-toggle {
+				border-color: transparent;
+				color: #fff !important;
+			}
+			footer .glyphicon {
+				font-size: 20px;
+				margin-bottom: 20px;
+				color: #f4511e;
+			}
+			.slideanim {visibility:hidden;}
+			.slide {
+				animation-name: slide;
+				-webkit-animation-name: slide;
+				animation-duration: 1s;
+				-webkit-animation-duration: 1s;
+				visibility: visible;
+			}
+			@keyframes slide {
+				0% {
+					opacity: 0;
+					transform: translateY(70%);
+				} 
+				100% {
+					opacity: 1;
+					transform: translateY(0%);
+				}
+			}
+			@-webkit-keyframes slide {
+				0% {
+					opacity: 0;
+					-webkit-transform: translateY(70%);
+				} 
+				100% {
+					opacity: 1;
+					-webkit-transform: translateY(0%);
+				}
+			}
+			@media screen and (max-width: 768px) {
+				.col-sm-4 {
+					text-align: center;
+					margin: 25px 0;
+				}
+				.btn-lg {
+					width: 100%;
+					margin-bottom: 35px;
+				}
+			}
+			@media screen and (max-width: 480px) {
+				.logo {
+					font-size: 150px;
+				}
+			}
+		</style>
 	</head>
-	<body>	
-		<nav class='navbar navbar-default navbar-fixed-top' id='home-nav'>
-		    <div class='container-fluid'>
-		        <div class='navbar-header'>
-		            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#responsive-menu' aria-expanded='false'>
-		                <span class='sr-only'>Toggle navigation</span>
-		                <span class='icon-bar'></span>
-		                <span class='icon-bar'></span>
-		                <span class='icon-bar'></span>
-		            </button>
-		        </div>
-		        <div class='collapse navbar-collapse' id='responsive-menu'>
-		            <ul class='nav navbar-nav navbar-right'>
-		            	<li><a href='#'>View More</a></li>
-		                <p class='navbar-text'>or</p>
-		                <li><a href='#'>Click Here</a></li>
-		            </ul>
-		        </div>
-		    </div>
+	<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">	
+		<nav class="navbar navbar-default navbar-fixed-top">
+  			<div class="container">
+    			<div class="navbar-header">
+      				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        				<span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>                        
+      				</button>
+      				<a class="navbar-brand" href="#myPage">Logo</a>
+    			</div>
+    			<div class="collapse navbar-collapse" id="myNavbar">
+			      	<ul class="nav navbar-nav navbar-right">
+				        <li><a href="#about">ABOUT</a></li>
+				        <li><a href="#services">SERVICES</a></li>
+				        <li><a href="#portfolio">PORTFOLIO</a></li>
+				        <li><a href="#pricing">PRICING</a></li>
+				        <li><a href="#contact">CONTACT</a></li>
+			      	</ul>
+    			</div>
+  			</div>
 		</nav>
 		<div class='content'>
 			<?= $this->fetch('content') ?>
 		</div>
+		<footer class="container-fluid text-center">
+  			<a href="#myPage" title="To Top">
+    			<span class="glyphicon glyphicon-chevron-up"></span>
+  			</a>
+  			<p>
+  				Bootstrap Theme Made By 
+  				<a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a>
+  			</p>
+		</footer>
+		<script>
+			$(document).ready(function(){
+			  // Add smooth scrolling to all links in navbar + footer link
+			  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+			    // Make sure this.hash has a value before overriding default behavior
+			    if (this.hash !== "") {
+			      // Prevent default anchor click behavior
+			      event.preventDefault();
+
+			      // Store hash
+			      var hash = this.hash;
+
+			      // Using jQuery's animate() method to add smooth page scroll
+			      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+			      $('html, body').animate({
+			      	scrollTop: $(hash).offset().top
+			      }, 900, function(){
+			      	
+			        // Add hash (#) to URL when done scrolling (default click behavior)
+			        window.location.hash = hash;
+			    });
+			    } // End if
+			});
+			  
+			  $(window).scroll(function() {
+			  	$(".slideanim").each(function(){
+			  		var pos = $(this).offset().top;
+
+			  		var winTop = $(window).scrollTop();
+			  		if (pos < winTop + 600) {
+			  			$(this).addClass("slide");
+			  		}
+			  	});
+			  });
+			})
+		</script>
 	</body>
 </html>
